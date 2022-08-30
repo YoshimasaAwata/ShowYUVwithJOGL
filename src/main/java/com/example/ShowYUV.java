46d273ef42a274db15f267bb161502c26dfb34db
Yoshiamasa Awata <yoshia.awata@nifty.ne.jp>
2022-08-30 21:04:39 +0900

    first commit

---
+++ src/main/java/com/example/ShowYUV.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/**
+ * YUVファイルを表示
+ *
+ */
+public class ShowYUV extends JFrame implements ActionListener {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+}
